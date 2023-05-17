a194dd1dce7f09c69120013ce75893f98d20efd4
RemigiuszZien <yamnik212@gmail.com>
2023-05-17 16:52:44 +0200

    .

--- wd11/zadaniaSeaborn.py
+++ wd11/zadaniaSeaborn.py
@@ -22,9 +22,30 @@ sns.set(rc={'figure.figsize':(8,8)})
 # wykres.add_legend()
 # wykres.figure.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
 # plt.show()
-data = pd.read_csv('iris.data',header=0,sep=',',decimal='.')
+# data = pd.read_csv('iris.data',header=0,sep=',',decimal='.')
+# df = pd.DataFrame(data)
+# sns.lineplot(data=df, x=df.index, y='sepal length', hue='class', palette=['yellow','green','blue'])
+# plt.xlabel("index")
+# plt.ylabel("sepal length")
+# plt.show()
+
+# data = {'a': np.arange(10),
+#         'c': np.random.randint(0,50,10),
+#         'd': np.random.randn(10)}
+# data['b'] = data['a'] + 10 * np.random.randn(10)
+# data['d'] = np.abs(data['d']) * 100
+# df = pd.DataFrame(data)
+# plot = sns.relplot(data=df, x='a', y='b', hue='c', palette='bright',
+#                    size='d', legend=True)
+# plot.set(xticks=data['a'])
+# plt.show()
+data = {'Kraj': ['Belgia', 'Indie', 'Brazylia', 'Polska'],
+        'Stolica': ['Bruksela', 'New Delhi', 'Brasilia', 'Warszawa'],
+        'Kontynent': ['Europa', 'Azja', 'Ameryka Poludniowa', 'Europa'],
+        'Populacja': [11190846, 1303171035, 207847528, 38675467]}
 df = pd.DataFrame(data)
-sns.lineplot(data=df, x=df.index, y='sepal length', hue='class', palette=['yellow','green','blue'])
-plt.xlabel("index")
-plt.ylabel("sepal length")
+plot = sns.barplot(data=df, x='Kontynent', y='Populacja', hue='Kontynent', estimator=np.sum,
+                   dodge=False, palette=['red','green','blue'],errorbar=None)
+plot.legend(title='Populacja na kontynentach')
+plot.set(title="Wykres słupkowy")
 plt.show()
